db12d520a847be52905f40b51eebf18a14d23fc1
zhouhuafei <1123486116@qq.com>
2022-08-27 16:07:56 +0800

    style: 💄 app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+// 第三方模块放这里
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { BrowserModule } from '@angular/platform-browser'
 
+// 自家的模块放这里
+import { routes } from './app.routes'
+
+// 自动引入的放这里
 import { AppComponent } from './app.component'
 import { HomeComponent } from './pages/home/home.component'
-
-import { routes } from './app.routes'
 
 @NgModule({
   declarations: [
