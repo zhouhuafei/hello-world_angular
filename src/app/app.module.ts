3aedce06b9ebf8bcbae9ed3ed44b1dfdf0ddc7b1
zhouhuafei <1123486116@qq.com>
2022-08-27 16:12:05 +0800

    feat: 🎸 增加mime 和 about路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,15 @@ import { routes } from './app.routes'
 // 自动引入的放这里
 import { AppComponent } from './app.component'
 import { HomeComponent } from './pages/home/home.component'
+import { MineComponent } from './pages/mine/mine.component'
+import { AboutComponent } from './pages/about/about.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MineComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { HomeComponent } from './pages/home/home.component'
+import { MineComponent } from './pages/mine/mine.component'
+import { AboutComponent } from './pages/about/about.component'
 
 const routes = [
   {
     path: '',
     children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'mine', component: MineComponent },
+      { path: 'about', component: AboutComponent }
     ]
   }
 ]
