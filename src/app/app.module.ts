d5ba244263cab38e74bded1d90d1fbfa118c3765
zhouhuafei <1123486116@qq.com>
2022-08-28 13:26:03 +0800

    style: 💄 LayoutComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,24 @@ import { routes } from './app.routes'
 
 // 自动引入的放这里
 import { AppComponent } from './app.component'
+import { LayoutComponent } from './components/layout/layout.component'
 import { HomeComponent } from './pages/home/home.component'
 import { MineComponent } from './pages/mine/mine.component'
 import { AboutComponent } from './pages/about/about.component'
 import { NotFoundComponent } from './pages/not-found/not-found.component'
 import { NavBarComponent } from './components/nav-bar/nav-bar.component'
 import { NavBarJsJumpComponent } from './components/nav-bar-js-jump/nav-bar-js-jump.component'
-import { LayoutComponent } from './components/layout/layout.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
     HomeComponent,
     MineComponent,
     AboutComponent,
     NotFoundComponent,
     NavBarComponent,
-    NavBarJsJumpComponent,
-    LayoutComponent
+    NavBarJsJumpComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router'
+import { LayoutComponent } from './components/layout/layout.component'
 import { HomeComponent } from './pages/home/home.component'
 import { MineComponent } from './pages/mine/mine.component'
 import { AboutComponent } from './pages/about/about.component'
 import { NotFoundComponent } from './pages/not-found/not-found.component'
-import { LayoutComponent } from './components/layout/layout.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
