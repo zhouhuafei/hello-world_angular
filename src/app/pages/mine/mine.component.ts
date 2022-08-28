148f0a7198349b255a8495c92a524c0a7ff5cb96
zhouhuafei <1123486116@qq.com>
2022-08-28 11:40:27 +0800

    feat: 🎸 在页面中获取路由数据

--- src/app/pages/mine/mine.component.ts
+++ src/app/pages/mine/mine.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-mine',
@@ -8,11 +8,12 @@ import { Router } from '@angular/router'
 })
 export class MineComponent implements OnInit {
 
-  constructor (private router: Router) {
+  constructor (private router: Router, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit (): void {
     console.log('this.router.url：', this.router.url)
+    console.log('this.activatedRoute.snapshot：', this.activatedRoute.snapshot)
   }
 
 }
