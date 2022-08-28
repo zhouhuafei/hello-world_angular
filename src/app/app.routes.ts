16ce758a18981cb530fe06c46c739bf4442b4fd8
zhouhuafei <1123486116@qq.com>
2022-08-28 12:29:39 +0800

    feat: 🎸 路由重定向

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
+import { Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
 import { MineComponent } from './pages/mine/mine.component'
 import { AboutComponent } from './pages/about/about.component'
 import { NotFoundComponent } from './pages/not-found/not-found.component'
 
-const routes = [
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, data: { title: '首页' } },
   { path: 'mine', component: MineComponent, data: { title: '我的' } },
   { path: 'about/:uid/behavior/:bid', component: AboutComponent, data: { title: '关于' } },
