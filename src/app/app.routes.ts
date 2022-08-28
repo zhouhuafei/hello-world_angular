5aa73adf302409cb388b1e50278f1894c440bea4
zhouhuafei <1123486116@qq.com>
2022-08-28 13:29:20 +0800

    feat: 🎸 注意：随着routes配置方式的不同，firstChild的值也不同，通过firstChild可以在layout中拿到当前路由的值。前提是需要监听路由变化。

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,16 +7,10 @@ import { NotFoundComponent } from './pages/not-found/not-found.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      { path: 'home', component: HomeComponent, data: { title: '首页' } },
-      { path: 'mine', component: MineComponent, data: { title: '我的' } },
-      { path: 'about/:uid/behavior/:bid', component: AboutComponent, data: { title: '关于' } },
-      { path: '**', component: NotFoundComponent, data: { title: '404' } }
-    ]
-  }
+  { path: 'home', component: HomeComponent, data: { title: '首页' } },
+  { path: 'mine', component: MineComponent, data: { title: '我的' } },
+  { path: 'about/:uid/behavior/:bid', component: AboutComponent, data: { title: '关于' } },
+  { path: '**', component: NotFoundComponent, data: { title: '404' } }
 ]
 
 export { routes }
