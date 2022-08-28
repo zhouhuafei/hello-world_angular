fdd17ca06082b42fd2523aec029782330a6b3641
zhouhuafei <1123486116@qq.com>
2022-08-28 16:58:15 +0800

    docs: ✏️ 文案调整

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -14,6 +14,7 @@ export class AuthGuard implements CanActivate {
     activatedRouteSnapshot: ActivatedRouteSnapshot,
     routerStateSnapshot: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    // ...TODO Observable
     // 返回Promise<boolean | UrlTree>和Observable<boolean | UrlTree>可以执行异步逻辑
     if ((activatedRouteSnapshot.queryParams as any).isLogin === '0') {
       return this.router.createUrlTree(['/login']) // 返回UrlTree进行路由跳转
